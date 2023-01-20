7c2f039c0d9135701d91ff1953922220c1c0e346
Matias <acostamatias25@gmail.com>
2023-01-19 21:07:19 -0300

    fix(client/component/Home): add component card y cambio idioma

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-function Home (props){
-    return(<div>
-        <h1>Bienvenidos a HenryBooks!</h1>
-        <h3>Aqui podras encontrar tus libros preferidos</h3>
-        {/* *aqui iria la searchbar* */}
-     
+import React from "react";
+import Card from "../Card/Card";
+import Style from "./Home.module.css";
 
-       <label>Filtrar categoria/genero</label> 
-       <select>
-            <option disabled>elige categoria</option>
-            <option value="terror">Terror</option>
-       </select>
-      <label>Orden:</label>
-      <select>
-          <option disabled>elige orden</option>
-          <option value="AZ">A-Z</option>
-          <option value="ZA">Z-A</option>
-          <option value="PD">Precio(mayor-menor)</option>
-          <option value="PA">Precio(menor-mayor</option>
-      </select>
+function Home(props) {
+  return (
+    <div className={Style.home_container}>
+      <h1>Welcome to HenryBooks!</h1>
+      <h3>Here you can find your favorite books</h3>
+      {/* *aqui iria la searchbar* */}
+      <ul className={Style.filter_container}>
+        <li>
+          Filter
+          <select>
+            <option disabled>select gender</option>
+            <option value="terror">Horror</option>
+          </select>
+        </li>
 
-    
-
-    </div>)
+        <li>
+          Order:
+          <select>
+            <option disabled>select order</option>
+            <option value="AZ">A-Z</option>
+            <option value="ZA">Z-A</option>
+            <option value="PD">Price(higher-smaller)</option>
+            <option value="PA">Price(smaller-higher)</option>
+          </select>
+        </li>
+      </ul>
+      <div className={Style.card_container}>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
-
-export default Home 
+export default Home;
