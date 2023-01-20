271853b47a022bdf70cfde598c50ba5ef4bdf4ed
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-20 20:48:16 -0300

    Integrantes agregados(About)

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,30 +1,67 @@
-import Footer from '../Footer/Footer'
 import Style from './About.module.css'
+import { members } from './members'
+
 
 function About() {
     return (
         <>
             <div className={Style.container}>
                 <div className={Style.titleContainer}>
-                    <h1 className={Style.title}>¿About us?</h1>
+                    <h1 className={Style.title}>About us</h1>
                 </div>
 
                 <div className={Style.info}>
 
-                    <h3 className={Style.text}>At Henry Books we specialize in selling books online.<br />
+                    <h2 className={Style.text}>At Henry Books we specialize in selling books online.<br />
                         Looking for a good experience for the user when buying.<br />
                         We have many books, both classic and modern and a wide variety of genres and authors.<br />
-                    </h3>
+                    </h2>
 
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
-                        Earum nihil culpa incidunt laboriosam fuga harum expedita ab deleniti dicta ducimus.
-                    </h4>
+                    <h3 className={Style.title2}>Now meet the people who made this possible. </h3>
+
+                    <p>Hello, we are students of the Full Stack Developers career, and in this web page we seek to demonstrate all our knowledge </p>
+
+                    <p> more information in our links to Linkedin and GitHub.</p>
+
+
 
                 </div>
 
+                <div className={Style.membersContainer}>
+                    {members.map((data) => {
+                        return (
+                            <div className={Style.data}>
+                                <div className={Style.nameContainer}>
+                                    <h5 className={Style.name}>{data.name}</h5>
+                                </div>
+
+                                <div className={Style.linksContainer}>
+
+                                    <a href={data.linkedin}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <ion-icon name='logo-linkedin' />
+                                    </a>
+
+                                    <a href={data.github}
+                                        target='_blank'
+                                        rel='noreferrer'
+                                    >
+                                        <ion-icon name='logo-github' />
+                                    </a>
+                                </div>
+                            </div>
+
+                        )
+
+                    })}
+
+                </div>
+
+
             </div>
 
-            <Footer />
         </>
     )
 }
---
+++ client/src/components/About/members.js
@@ -0,0 +1,45 @@
+export const members = [
+    {
+        name: 'Gonzalo Sancho',
+        linkedin: 'https://www.linkedin.com/in/gonzalo-sancho-181a78237/',
+        github: 'https://github.com/gsancho10'
+    },
+
+    {
+        name: 'Adrian Jesus Perez',
+        linkedin: 'https://www.linkedin.com/in/adrian4058',
+        github: 'https://github.com/adrian4058'
+    },
+
+    {
+        name: 'Matias Edgardo Acosta',
+        linkedin: 'https://www.linkedin.com/in/matias-acosta-32c',
+        github: 'https://github.com/Matias257987'
+    },
+
+    {
+        name: 'Juan Manuel Carreras',
+        linkedin: 'www.linkedin.com/in/manuel-carreras',
+        github: 'https://github.com/JuanMaCarreras'
+    },
+
+    {
+        name: 'Pedro Torcuato Insausti',
+        linkedin: 'https://www.linkedin.com/in/pedro-torcuato-insausti-54958159',
+        github: 'https://github.com/pedroinsa'
+    },
+
+    {
+        name: 'Danilo Urrego',
+        linkedin: 'https://www.linkedin.com/in/joimar-danilo-urrego-david-7a5581248',
+        github: 'https://github.com/DaniloVZR'
+    },
+
+    {
+        name: 'Santiago Colombi Canal Chavez',
+        linkedin: 'https://www.linkedin.com/',
+        github: 'https://github.com/scch94'
+    }
+
+
+]
