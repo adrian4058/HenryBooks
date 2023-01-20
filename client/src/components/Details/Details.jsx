11aac4e52a30494dbbab6d4c6cca46733ea774b8
Matias <acostamatias25@gmail.com>
2023-01-19 21:05:38 -0300

    fix(client/component/detail): se agrego dispatch, id a ussEffect falta testear

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,27 +1,29 @@
-import React,{useEffect} from 'react';
-import {useParams} from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {getBookDetail, cleanDetail }from '../../actions/index'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getBookDetail, cleanDetail } from "../../actions/index";
 
-function Details (props){
-    const {id} = useParams()
-    const details = useSelector(state=> state.detail)
-    const dispatch = useDispatch()
-    useEffect(()=>{
-     dispatch(getBookDetail(id))
-     return ()=> dispatch(cleanDetail()) },[])
+function Details(props) {
+  const { id } = useParams();
+  const details = useSelector((state) => state.detail);
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getBookDetail(id));
+    return () => dispatch(cleanDetail());
+  }, [dispatch, id]);
 
- return (<div>
-     {details.name ? <h1>{details.name}</h1>: <h1>Loading</h1>}
-    {details.image && <img src={details.image} alt="imagen-libro"/>}
-    {details.autor && <h4>{details.autor}</h4>}
-    {details.editorial && <h4>Editorial: </h4>}
-    {details.editorial && <p>{details.editorial}</p>}
-    {details.genero && <h4>Genero: {details.genero}</h4> }
-    {details.price  && <h4>Precio: {details.price}</h4> }
-
-
-       </div>)
-
+  return (
+    <div>
+      {details.name ? <h1>{details.name}</h1> : <h1>Loading</h1>}
+      {details.image && <img src={details.image} alt="imagen-libro" />}
+      {details.autor && <h4>{details.autor}</h4>}
+      {details.editorial && <h4>Editorial: </h4>}
+      {details.editorial && <p>{details.editorial}</p>}
+      {details.genero && <h4>Genero: {details.genero}</h4>}
+      {details.price && <h4>Precio: {details.price}</h4>}
+    </div>
+  );
 }
+
+export default Details;
