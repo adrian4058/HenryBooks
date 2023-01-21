335af0964c81a917c4e8d5ce8e90b4de3954a42e
Danilo Urrego <daniloud2809@gmail.com>
2023-01-21 13:19:12 -0500

    index.js fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
@@ -8,8 +8,9 @@ import { store } from "./store/index";
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <Auth0Provider
       domain={domain}
@@ -19,7 +20,6 @@ ReactDOM.render(
       <App />
     </Auth0Provider>    
   </Provider>,
-  document.getElementById("root")
 );
 
 
