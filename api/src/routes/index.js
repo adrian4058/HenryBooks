c248ec75a6194cef484c09bcdbffc926df6e5ef0
adrian4058 <adrian_2016_@outlook.es>
2023-01-25 11:16:33 -0300

    ajustes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const autores = require("./autorRoutes");
 const books = require("./librosRoutes");
-const resenas=require("./resena.routes")
+const resenas = require("./resenaRoutes");
 
-
-router.use("/book", books)
-router.use('/autores',autores)
-router.use("/resena", resenas)
-
+router.use("/book", books);
+router.use("/autores", autores);
+router.use("/resena", resenas);
 
 module.exports = router;
--- api/src/routes/resena.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router} = require("express");
-const { crearResena,resenasPorIDl } = require("../controllers/resenasControllers");
-const router = Router();
-
-router.post('/',crearResena)
-router.get('/',resenasPorIDl)
-
-
-module.exports=router
