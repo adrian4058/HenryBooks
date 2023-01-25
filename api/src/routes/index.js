df7908fecbc341cb15031e46179ca89ed3e07d23
adrian4058 <adrian_2016_@outlook.es>
2023-01-25 11:14:34 -0300

    payment route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const autores = require("./autorRoutes");
 const books = require("./librosRoutes");
+const payment = require("./paymentRoute");
 
 router.use("/book", books);
 router.use("/autores", autores);
+router.use("/payment", payment);
 
 module.exports = router;
