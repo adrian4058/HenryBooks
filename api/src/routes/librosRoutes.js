ca87c7d5fe2bf98b3fbbcd91813b282ca31da12e
santiago canal <santycanal_06@hotmail.com>
2023-01-26 07:58:33 -0500

    corrigiendo

--- api/src/routes/librosRoutes.js
+++ api/src/routes/librosRoutes.js
@@ -10,7 +10,7 @@ const {
 
 router.post("/", createBook);
 router.get("/", allBooks);
-router.get("/:id", findBook); //ruta no testeada
+router.get("/orden/:id", findBook); //ruta no testeada
 router.put("/:id", updateBook); //ruta no testeada
 
 router.get("/orden/az",ordenAlfabetico)
