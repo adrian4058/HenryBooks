05302499fb4ae4592ae4dfe5a38b9eaa2221cef6
adrian4058 <adrian_2016_@outlook.es>
2023-01-27 01:19:59 -0300

    ajustes

--- api/src/routes/paymentRoute.js
+++ api/src/routes/paymentRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { linkPayment } = require("../controllers/paymentController");
 const router = express.Router();
 
-router.post("/", linkPayment);
+router.get("/", linkPayment);
 
 module.exports = router;
