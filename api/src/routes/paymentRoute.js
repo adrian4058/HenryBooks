511e090c7d5e785a366c5a1c4757e125d484c9d2
adrian4058 <adrian_2016_@outlook.es>
2023-01-28 16:48:03 -0300

    adjustes y pasarela conectada al front

--- api/src/routes/paymentRoute.js
+++ api/src/routes/paymentRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { linkPayment } = require("../controllers/paymentController");
 const router = express.Router();
 
-router.get("/", linkPayment);
+router.post("/", linkPayment);
 
 module.exports = router;
