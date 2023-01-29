2117d65458f56a5b6eb888eb78ff26abf61bba4e
Danilo Urrego <daniloud2809@gmail.com>
2023-01-28 20:27:09 -0500

    Crear, Editar, y Desactivar Libros

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import About from "./components/About/About";
 import Home from "./components/Home/Home";
 import Details from "./components/Details/Details";
 import Team from "./components/Team/Team";
-import Dashboard from "./components/Dashboard/Dashboard";
+import DashboardRoutes from "./components/Dashboard/DashboardRoutes/DashboardRoutes";
+import DashboardBooks from "./components/Dashboard/DashboardComponents/DashboardBooks/DashboardBooks";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 
@@ -19,10 +20,10 @@ function App() {
             <Route exact path="/home" component={Home} />
             <Route exact path="/about-us" component={About} />
             <Route exact path="/team" component={Team} />
-            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/dashboard" component={DashboardRoutes} />
+            <Route exact path="/dashboard-books" component={DashboardBooks} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-
             <Route path="/books/:id" component={Details} />
           </Route>
         </Switch>
---
+++ client/src/components/Dashboard/DashboardComponents/DashboardBooks/BookCards/BookCards.jsx
@@ -0,0 +1,184 @@
+import BookCard from "../BookCard/BookCard";
+import { getAllBooks } from "../../../../../actions";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewBook, emptyMessage } from "../../../../../actions";
+import Modal from "react-modal";
+import "./BookCards.css";
+
+const BookCards = () => {
+
+  const dispatch = useDispatch();
+  const allBooks = useSelector(state => state.allBooks)
+
+  useEffect(() => {
+    dispatch(getAllBooks())
+  }, [dispatch])
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+    dispatch(emptyMessage());
+    window.location.reload();
+  }
+
+  return (
+
+    <div className="BookCards">
+      <div>
+        <button onClick={openModal}>Crear Nuevo Libro</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          ariaHideApp={false}
+        >
+          <FormCreateBook />
+        </Modal>
+      </div>
+      <div className="Dash-BookCards">
+        {
+          allBooks.map(elem =>
+            <div className="Dash-BookCards-Container" key={elem.id}>
+              <BookCard
+                genre={elem.genero}
+                editorial={elem.editorial}
+                stock={elem.stock}
+                state={elem.estado}
+                author={elem.Autor.nombre}
+                image={elem.image}
+                name={elem.name}
+                id={elem.id}
+                price={elem.price} />
+            </div>)}
+      </div>
+    </div>
+  )
+}
+
+
+const FormCreateBook = () => {
+  const message = useSelector(state => state.message)
+  const dispatch = useDispatch();
+  const [book, setBook] = useState({
+    name: "",
+    autor: "",
+    editorial: "",
+    reviews: [],
+    genero: "",
+    stock: "",
+    price: "",
+    image: "",
+  });
+
+  const handleForm = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmitCreate = (e) => {
+    e.preventDefault();
+    if (book.name === "" || book.autor === "" || book.editorial === "" || book.genero === "" || book.stock === "" || book.price === "" || book.image === "")
+      return alert('You must complete the fields');
+    dispatch(addNewBook(book));
+    setBook({
+      name: "",
+      autor: "",
+      editorial: "",
+      reviews: [],
+      genero: "",
+      stock: "",
+      price: "",
+      image: "",
+    })
+  };
+
+  return (
+    <form className="BC-CreateBooks" onSubmit={handleSubmitCreate}>
+      <div className="BC-CreateBooks-input">
+        <label>Book Name: </label>
+        <input
+          className="Input-CreateBooks"
+          type="text"
+          name="name"
+          value={book.name}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Author: </label>
+        <input
+          className="Input-CreateBooks"
+          type="text"
+          name="autor"
+          value={book.autor}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Editorial: </label>
+        <input
+          className="Input-CreateBooks"
+          type="text"
+          name="editorial"
+          value={book.editorial}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Genre: </label>
+        <input
+          className="Input-CreateBooks"
+          type="text"
+          name="genero"
+          value={book.genero}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Stock: </label>
+        <input
+          className="Input-CreateBooks"
+          min="0"
+          type="number"
+          name="stock"
+          value={book.stock}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Price: </label>
+        <input
+          className="Input-CreateBooks"
+          min="0"
+          type="number"
+          name="price"
+          value={book.price}
+          onChange={handleForm}
+        />
+      </div>
+      <div className="BC-CreateBooks-input">
+        <label>Image: </label>
+        <input
+          className="Input-CreateBooks"
+          type="text"
+          name="image"
+          value={book.image}
+          onChange={handleForm}
+        />
+      </div>
+      <input type="submit" value="Crear Nuevo Libro" />
+      {
+        message.length ? <div>Book Created Succesfully</div> : null
+      }
+    </form>
+  )
+}
+
+export default BookCards;
