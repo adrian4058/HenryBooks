a1348300c12365c37531faac76ff7913021484bf
JuanMaCarrearas <manuucarreras@gmail.com>
2023-01-29 22:26:41 -0300

    ChatBot

--- client/src/components/ChatBot/Chat.jsx
+++ client/src/components/ChatBot/Chat.jsx
@@ -15,7 +15,7 @@ function Chat() {
                     className={Style.btn}>Chatbot</button>
 
                 <div className={isVisible ? '' : Style.hide}>
-                    <ChatBot
+                    <ChatBot className={Style.chatContainer}
                         steps={[
                             {
                                 id: 'intro',
@@ -30,29 +30,6 @@ function Chat() {
                             {
                                 id: 'respuesta-nombre',
                                 message: 'Hola {previousValue}, un gusto conocerte',
-                                trigger: 'opciones',
-                            },
-                            {
-                                id: 'opciones',
-                                message: '¿En qué puedo ayudarte hoy?',
-                                trigger: 'opciones-ayuda',
-
-                            },
-                            {
-                                id: 'opciones-ayuda',
-                                options: [
-                                    { value: 'libros', label: 'Libros', trigger: 'libros' },
-                                    { value: 'compra', label: 'Compra', trigger: 'compra' },
-                                ],
-                            },
-                            {
-                                id: 'libros',
-                                message: '¿Qué tipo de libros estás buscando?',
-                                end: true,
-                            },
-                            {
-                                id: 'compra',
-                                message: '¿Qué deseas comprar hoy?',
                                 end: true,
                             },
                         ]} />
