2c48b8449c4574dd25c86227a41a76a674bf2a5b
adrian4058 <adrian_2016_@outlook.es>
2023-01-31 00:18:00 -0300

    Ajustes

--- api/src/routes/librosRoutes.js
+++ api/src/routes/librosRoutes.js
@@ -10,10 +10,10 @@ const {
 const {verifyToken,isAdmin} =require('../middleware')
 
 
-router.post("/",[verifyToken,isAdmin], createBook);
+router.post("/", createBook); //router.post("/",[verifyToken,isAdmin], createBook);
 router.get("/", allBooks);
-router.get("/:id", findBook); //ruta no testeada
-router.put("/:id", updateBook); //ruta no testeada
+router.get("/:id", findBook); 
+router.put("/:id", updateBook); 
 
 router.get("/orden/az",ordenAlfabetico)
 
