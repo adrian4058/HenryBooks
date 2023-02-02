cd4c9a8f5e729c0e02dda29fb52c66f8ff7d1441
Danilo Urrego <daniloud2809@gmail.com>
2023-02-01 20:53:53 -0500

    Design login & register, page default

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -8,6 +8,8 @@ import Cookies from "universal-cookie";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Api from "../../Global";
+import { AiOutlineLogin, AiFillHome } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 
 const Register = () => {
   const cookie = new Cookies();
@@ -80,27 +82,41 @@ const Register = () => {
   });
 
   return (
-    <form noValidate onSubmit={handleSubmit}>
-      <div className="register">
-        <div className="all-content">
-          <div className="register-container">
-            <h1>Register here!</h1>
-            <div className="content">
-              <label htmlFor="nombre">Name:</label>
-              <input
-                type="text"
-                placeholder="Nombre"
-                {...getFieldProps("nombre")}
-                className={`${touched.nombre &&
-                  errors.nombre &&
-                  "error_input"}`}
-              />
-              <div className="adv">
-                {touched.nombre && errors.nombre && (
-                  <span className="error">{errors.nombre}</span>
-                )}
-              </div>
-              {/* <label htmlFor="lastname">Lastname:</label>
+    <form className="Register" noValidate onSubmit={handleSubmit}>
+      <div className="Register-content">
+
+        <div className="Login-home-container">
+          <Link to="/home">
+            <button className="Login-home__btn">
+              <AiFillHome />
+              <span>
+                Home
+              </span>
+            </button>
+          </Link>
+        </div>
+
+        <h1>Register</h1>
+
+        <div className="Register-form">
+          <div className="Register-input">
+            <label htmlFor="nombre">Name:</label>
+            <input
+              type="text"
+              placeholder="Nombre"
+              {...getFieldProps("nombre")}
+              className={`Login-Register__input ${touched.nombre &&
+                errors.nombre &&
+                "error_input"}`}
+            />
+            <div className="adv">
+              {touched.nombre && errors.nombre && (
+                <span className="error">{errors.nombre}</span>
+              )}
+            </div>
+          </div>
+
+          {/* <label htmlFor="lastname">Lastname:</label>
               <input
                 type="text"
                 placeholder="Lastname"
@@ -114,83 +130,84 @@ const Register = () => {
                   <span className="error">{errors.lastname}</span>
                 )}
               </div> */}
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                placeholder="E-mail"
-                {...getFieldProps("email")}
-                className={`${touched.email && errors.email && "error_input"}`}
-              />
-              <div className="adv">
-                {touched.email && errors.email && (
-                  <span className="error">{errors.email}</span>
-                )}
-              </div>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                placeholder="Password"
-                {...getFieldProps("password")}
-                className={`${touched.password &&
-                  errors.password &&
-                  "error_input"}`}
-              />
-              <div className="adv">
-                {touched.password && errors.password && (
-                  <span className="error">{errors.password}</span>
-                )}
-              </div>
-              <div>
-                <label htmlFor="rol">Select an option:</label>
-
-                <select id="rol" onChange={handleChange}>
-                  <option value="">--- Select ---</option>
-                  <option value="admin">Admin</option>
-                  <option value="user">User</option>
-                </select>
-              </div>
-
-              {touched.rol && errors.rol && (
-                <span className="error">{errors.rol}</span>
+          <div className="Register-input">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              placeholder="E-mail"
+              {...getFieldProps("email")}
+              className={`Login-Register__input ${touched.email && errors.email && "error_input"}`}
+            />
+            <div className="adv">
+              {touched.email && errors.email && (
+                <span className="error">{errors.email}</span>
               )}
-              <br />
-              <div>
-                <button className="botsub" type="submit">
-                  Create User
-                </button>
-              </div>
-              <p>Or</p>
-              {/* <GoogleRegister /> */}
-              <div className="login">
-                {isAuthenticated ? (
-                  <button
-                    className="navbar-btn__option"
-                    onClick={() => logout({ returnTo: window.location.origin })}
-                  >
-                    Logout
-                  </button>
-                ) : (
-                  <button
-                    className="navbar-btn__option"
-                    onClick={() => loginWithPopup()}
-                  >
-                    <i class="fa-brands fa-google"></i>
-                  </button>
-                )}
-              </div>
-              <br />
-              <div className="navbar-options__link">
-                <Link to="/home">
-                  <button className="form-btn">
-                    <b>Home</b>
-                  </button>
-                </Link>
-              </div>
             </div>
           </div>
+
+          <div className="Register-input">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              placeholder="Password"
+              {...getFieldProps("password")}
+              className={`Login-Register__input ${touched.password &&
+                errors.password &&
+                "error_input"}`}
+            />
+            <div className="adv">
+              {touched.password && errors.password && (
+                <span className="error">{errors.password}</span>
+              )}
+            </div>
+          </div>
+
+          <div className="Register-input">
+            <label htmlFor="rol">Select an option:</label>
+            <select className="Register-input__select" id="rol" onChange={handleChange}>
+              <option className="Register-input__option" value="">--- Select ---</option>
+              <option className="Register-input__option" value="admin">Admin</option>
+              <option className="Register-input__option" value="user">User</option>
+            </select>
+          </div>
+
+          {touched.rol && errors.rol && (
+            <span className="error">{errors.rol}</span>
+          )}
+
+          <div>
+            <button className="Register-create__btn" type="submit">
+              Create User
+            </button>
+          </div>
+        </div>
+
+        <p>Or</p>
+
+        {/* <GoogleRegister /> */}
+        <div className="Register-google">
+          {isAuthenticated ? (
+            <button
+              className="navbar-btn__option"
+              onClick={() => logout({ returnTo: window.location.origin })}
+            >
+              Logout
+            </button>
+          ) : (
+            <button
+              className="Login-google__btn"
+              onClick={() => loginWithPopup()}
+            >
+              <FcGoogle />
+              <span>
+                Continue With Google
+              </span>
+            </button>
+
+          )}
         </div>
       </div>
-    </form>
+    </form >
   );
 };
 
