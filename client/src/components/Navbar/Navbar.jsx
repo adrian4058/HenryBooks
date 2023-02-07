013325493034537c72167c6589a99462ed10ceae
Danilo Urrego <daniloud2809@gmail.com>
2023-02-07 17:18:44 -0500

    nav cart option & profile styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,19 +6,21 @@ import "./Navbar.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../actions/index";
+import { HiShoppingCart } from "react-icons/hi";
+
 
 function NavBar() {
   let dispatch = useDispatch();
   let token2 = useSelector((state) => state.token);
   let usuario2 = useSelector((state) => state.userProfile);
-  let [token,setToken]=useState(token2)
-  let [usuario,setUsuario] =useState(usuario2);
+  let [token, setToken] = useState(token2)
+  let [usuario, setUsuario] = useState(usuario2);
   const { logout } = useAuth0();
   const cookies = new Cookies();
-  useEffect(()=>{
+  useEffect(() => {
     setToken(token2)
     setUsuario(usuario2)
-  },[usuario2][token2])
+  }, [usuario2][token2])
 
   function cerrrarSesion(e) {
     e.preventDefault();
@@ -97,12 +99,20 @@ function NavBar() {
         <div>
           {Object.keys(usuario).length > 0 ? (
             <div>
-              <button
-                className="navbar-btn__option"
-                onClick={(e) => cerrrarSesion(e)}
-              >
-                Logout
-              </button>
+              <div className="user-btns">
+                <Link to="/cart">
+                  <button className="navbar-btn__option cart-btn">
+                    <b>Cart</b>
+                    <HiShoppingCart />
+                  </button>
+                </Link>
+                <button
+                  className="navbar-btn__option"
+                  onClick={(e) => cerrrarSesion(e)}
+                >
+                  Logout
+                </button>
+              </div>
               {cookies.get("email") ? (
                 <a href="/profile/edit">
                   {/* <label className="user-name">
--- client/src/components/userProfile/userProfile.jsx
+++ client/src/components/userProfile/userProfile.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUser, updateUser} from "../../actions";
+import { getUser, updateUser } from "../../actions";
 import NavBar from "../Navbar/Navbar";
 import "./userProfile.css";
 import Modal from "react-modal"
@@ -10,209 +10,230 @@ import { countries } from "../../utils/countries"
 // import Api from "../Global";
 // const url = Api.Url;
 
-export default function UserProfile () {
+export default function UserProfile() {
 
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.userProfile);
-    const message = useSelector(state => state.message)
-    
-
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    useEffect(() => {
-      dispatch(getUser());
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userProfile);
+  const message = useSelector(state => state.message)
 
 
-    const openEditModal = () => {
-      setModalIsOpen(true);
-    }
-  
-    const closeEditModal = () => {
-      setModalIsOpen(false);
-      
-    }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
 
 
-    const [editUser, setEditUser] = useState({
-      
-      nombre: user.nombre,
-      email: user.email,
-      password: user.password,
-      direccion: user.direccion,
-      pais: user.pais,
-      ciudad: user.ciudad,
-      
-    });
+  const openEditModal = () => {
+    setModalIsOpen(true);
+  }
 
-    const handleUpdateForm = (e) => {
-      setEditUser({
-        ...editUser,
-        [e.target.name]: e.target.value
-      })
-    };
+  const closeEditModal = () => {
+    setModalIsOpen(false);
 
-    const handleSubmitUpdate = (e) => {
-      e.preventDefault();
-      if (editUser.name === "" || editUser.email === "" || editUser.password === "")
-        return alert('This fields cannot be empty')
-      dispatch((updateUser(user.id, editUser)))
-      // window.location.reload()
-     
-    }
+  }
+
+
+  const [editUser, setEditUser] = useState({
+
+    nombre: user.nombre,
+    email: user.email,
+    password: user.password,
+    direccion: user.direccion,
+    pais: user.pais,
+    ciudad: user.ciudad,
+
+  });
+
+  const handleUpdateForm = (e) => {
+    setEditUser({
+      ...editUser,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmitUpdate = (e) => {
+    e.preventDefault();
+    if (editUser.name === "" || editUser.email === "" || editUser.password === "")
+      return alert('This fields cannot be empty')
+    dispatch((updateUser(user.id, editUser)))
+    // window.location.reload()
+
+  }
 
   const logout = (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
     // setAuth(false);
   };
-  
+
 
   const imagedefault = "https://thumbs.dreamstime.com/z/icono-del-usuario-en-estilo-plano-de-moda-aislado-fondo-gris-s%C3%ADmbolo-123663211.jpg"
 
 
 
-return (
-    
-        <div className="info">
-          <NavBar />
-          <div className="alldata">
-          <div className="headatos">
-            <h3 className="tituloAccount"> Datos personales </h3>
-           
-          </div>
+  return (
 
-          <div className="usuario">
-            <div className="usuarioimg">
-              <img
-                className="datoimg"
-                name="image"
-                value={user.image}
-                src={user.image? user.image : imagedefault}
-                width="120px"
-              ></img>
-            </div>
-            <div>
-            <h3 className="dato" name="id" value={user.id}>
-                {" "}
-                ID: {user.id}{" "}
-              </h3>
+    <div className="Profile">
+      <NavBar />
+      <div className="Profile-user__content">
+        <div className="headatos">
+          <h3 className="tituloAccount">Personal Info</h3>
+        </div>
 
-              <h3 className="dato" name="name" value={user.nombre}>
-                {" "}
-                Name: {user.nombre}{" "}
-              </h3>
-            
-              <h3 className="dato" name="email" value={user.email}>
-                
-                {" "}
-                Mail: {user.email}{" "}
-              </h3>
-              <h3 className="dato" name="password" value={user.password}>
-                {" "}
-                Password: **********{" "}
-              </h3>
-              <h3 className="dato" name="direccion" value={user.direccion}>
-                {" "}
-                Address: {user.direccion}{" "}
-              </h3>
-              <h3 className="dato" name="pais" value={user.pais}>
-                {" "}
-                Country: {user.pais}{" "}
-              </h3>
-              <h3 className="dato" name="ciudad" value={user.ciudad}>
-                {" "}
-                City/State: {user.ciudad}{" "}
-              </h3>
-            </div>
+        <div className="Profile-user">
+          <div className="Profile-user__img">
+            <img
+              className="datoimg"
+              name="image"
+              value={user.image}
+              src={user.image ? user.image : imagedefault}
+              width="120px"
+            />
           </div>
-          <button className="logoutBtn" onClick={(e) => logout(e)}>
+          <div className="Profile-user__info">
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="id" value={user.id}>
+                <span>UserID:</span>
+                <span>{user.id}</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="name" value={user.nombre}>
+                <span>Name:</span>
+                <span>{user.nombre}</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="email" value={user.email}>
+                <span>Email:</span>
+                <span>{user.email}</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="password" value={user.email}>
+                <span>Password:</span>
+                <span>**********</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="direccion" value={user.direccion}>
+                <span>Address:</span>
+                <span>{user.direccion}</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="pais" value={user.pais}>
+                <span>Country:</span>
+                <span>{user.pais}</span>
+              </h5>
+            </div>
+
+            <div className="Profile-usuario__data">
+              <h5 className="Profile-info" name="ciudad" value={user.ciudad}>
+                <span>City/State:</span>
+                <span>{user.ciudad}</span>
+              </h5>
+            </div>
+
+          </div>
+        </div>
+        <div className="Profile-btns">
+          <button className="Profile-btn" onClick={openEditModal}>Edit profile</button>
+          <button className="Profile-btn" onClick={(e) => logout(e)}>
             Logout
           </button>
         </div>
-        <button  onClick={openEditModal}>Edit profile</button>
-          <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={closeEditModal}
-            ariaHideApp={false}
-            className="modal-form"
-          >
-            <form className="BC-CreateBooks" onSubmit={handleSubmitUpdate}>
-            
-              <div className="BC-CreateBooks-input">
-                <label>Name: </label>
-                <input
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="nombre"
-                  value={editUser.nombre}
-                  onChange={handleUpdateForm}
-                />
-              </div>
-              <div className="BC-CreateBooks-input">
-                <label>Email: </label>
-                <input
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="email"
-                  value={editUser.email}
-                  onChange={handleUpdateForm}
-                />
-              </div>
-              <div className="BC-CreateBooks-input">
-                <label>Password: </label>
-                <input
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="password"
-                  value={editUser.password}
-                  onChange={handleUpdateForm}
-                />
-              </div>
-              <div className="BC-CreateBooks-input">
-                <label>Address: </label>
-                <input
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="direccion"
-                  value={editUser.direccion}
-                  onChange={handleUpdateForm}
-                />
-              </div>
-              <div className="BC-CreateBooks-input">
-                <label>Country: </label>
-                <select
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="pais"
-                  value={editUser.pais}
-                  onChange={handleUpdateForm} >
-                    <option> </option>
-                    {countries.map((data) => (
-                      <option value={data.id}>{data.name_es}</option>
-                    ))}
-                  </select>
-              </div>
-              <div className="BC-CreateBooks-input">
-                <label>City: </label>
-                <input
-                  className="Input-CreateBooks"
-                  type="text"
-                  name="ciudad"
-                  value={editUser.ciudad}
-                  onChange={handleUpdateForm}
-                >
+      </div>
 
-                </input>
-              </div>
-              {/* <input className="input-btn-dash-form" type="submit" value="Edit User" /> */}
-              <button onClick={(e) => handleSubmitUpdate(e)}> Edit User</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeEditModal}
+        ariaHideApp={false}
+        className="modal-form"
+      >
+        <form className="BC-CreateBooks" onSubmit={handleSubmitUpdate}>
 
-              {
-                message.length ? <div>Profile Edited Succesfully</div> : null
-              }
-              </form>
-              </Modal>
-        </div>
+          <div className="BC-CreateBooks-input">
+            <label>Name: </label>
+            <input
+              className="Input-CreateBooks"
+              type="text"
+              name="nombre"
+              value={editUser.nombre}
+              onChange={handleUpdateForm}
+            />
+          </div>
+          <div className="BC-CreateBooks-input">
+            <label>Email: </label>
+            <input
+              className="Input-CreateBooks"
+              type="text"
+              name="email"
+              value={editUser.email}
+              onChange={handleUpdateForm}
+            />
+          </div>
+          <div className="BC-CreateBooks-input">
+            <label>Password: </label>
+            <input
+              className="Input-CreateBooks"
+              type="text"
+              name="password"
+              value={editUser.password}
+              onChange={handleUpdateForm}
+            />
+          </div>
+          <div className="BC-CreateBooks-input">
+            <label>Address: </label>
+            <input
+              className="Input-CreateBooks"
+              type="text"
+              name="direccion"
+              value={editUser.direccion}
+              onChange={handleUpdateForm}
+            />
+          </div>
+          <div className="BC-CreateBooks-input">
+            <label>Country: </label>
+            <select
+              className="Input-CreateBooks"
+              type="text"
+              name="pais"
+              value={editUser.pais}
+              onChange={handleUpdateForm} >
+              <option> </option>
+              {countries.map((data) => (
+                <option value={data.id}>{data.name_es}</option>
+              ))}
+            </select>
+          </div>
+          <div className="BC-CreateBooks-input">
+            <label>City: </label>
+            <input
+              className="Input-CreateBooks"
+              type="text"
+              name="ciudad"
+              value={editUser.ciudad}
+              onChange={handleUpdateForm}
+            >
+
+            </input>
+          </div>
+          {/* <input className="input-btn-dash-form" type="submit" value="Edit User" /> */}
+          <button onClick={(e) => handleSubmitUpdate(e)}> Edit User</button>
+
+          {
+            message.length ? <div>Profile Edited Succesfully</div> : null
+          }
+        </form>
+      </Modal>
+    </div>
   );
 }
