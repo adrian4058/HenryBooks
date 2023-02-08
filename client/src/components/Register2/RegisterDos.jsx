5ec8acb583345b016b6dd07b9dc6b79ac12032b5
adrian4058 <adrian_2016_@outlook.es>
2023-02-08 17:06:41 -0300

    Cambios

--- client/src/components/Register2/RegisterDos.jsx
+++ client/src/components/Register2/RegisterDos.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { AiOutlineLogin, AiFillHome } from "react-icons/ai";
 import Auth0 from "../Auth0/Auth0";
-import Swal from "sweetalert2"
+import Swal from "sweetalert2";
+import Api from "../../Global";
 
 export const RegisterDos = () => {
   let [input, setInput] = useState({ name: "", email: "", password: "" });
@@ -24,7 +25,7 @@ export const RegisterDos = () => {
       email: input.email,
       password: input.password,
     };
-    let url = "http://localhost:5685/auth/signup";
+    let url = Api.Url + "/auth/signup";
     let status;
     fetch(url, {
       headers: {
@@ -43,20 +44,20 @@ export const RegisterDos = () => {
         if (status === 200) {
           setHome(true);
           Swal.fire({
-            icon: 'success',
-            title: 'User registered'
-          })
+            icon: "success",
+            title: "User registered",
+          });
         } else {
           Swal.fire({
-            icon: 'error',
-            title: 'Name or Email already exist',
+            icon: "error",
+            title: "Name or Email already exist",
             showClass: {
-              popup: 'animate__animated animate__fadeInDown'
+              popup: "animate__animated animate__fadeInDown",
             },
             hideClass: {
-              popup: 'animate__animated animate__fadeOutUp'
-            }
-          })
+              popup: "animate__animated animate__fadeOutUp",
+            },
+          });
         }
       })
       .catch((error) => console.error(error));
@@ -84,9 +85,9 @@ export const RegisterDos = () => {
     if (!input.password) {
       error.password = "password es requerido";
     } else if (input.password.length < 6) {
-      error.password = "Debe tener mas de 6 caracteres"
-    }else if ((/[^A-Za-z0-9 ]+/g.test(input.password))) {
-      error.password = "No debe contener caracteres especiales"
+      error.password = "Debe tener mas de 6 caracteres";
+    } else if (/[^A-Za-z0-9 ]+/g.test(input.password)) {
+      error.password = "No debe contener caracteres especiales";
     }
     return error;
   }
--- client/src/components/ShoppingCart/ShoppingCart.jsx
+++ client/src/components/ShoppingCart/ShoppingCart.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllBooks, TYPES } from "../../actions";
 import axios from "axios";
-import Card from "../Card/Card";
 import Footer from "../Footer/Footer";
 import NavBar from "../Navbar/Navbar";
 import CartItem from "./CartItem";
@@ -69,7 +68,7 @@ const ShoppingCart = () => {
     <div>
       <NavBar />
       <h3> Your Books in Cart</h3>
-      
+
       <article className="box">
         <button onClick={clearCart}>Limpiar Carrito</button>
         {cart.map((item, index) => (
