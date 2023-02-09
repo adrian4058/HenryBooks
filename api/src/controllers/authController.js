1410a61812560a8df1e84598b0b2b730fbbbc4eb
adrian4058 <adrian_2016_@outlook.es>
2023-02-09 02:50:52 -0300

    nodemailer

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -1,50 +1,61 @@
-const {Usuario} = require("../db");
-const jwt =require('jsonwebtoken');
-async function signUp (req,res){
-    let {nombre, email, password,rol,estado}=req.body
-    let existe = await Usuario.findOne({ where: {nombre} });
-    if(existe){
-        return res.status(404).send({msj:`User with name: ${nombre} already exist!`})
-    }
-    existe =await Usuario.findOne({ where: {email} });
-    if(existe){
-        return res.status(404).send({msj:`E-mail: ${email} already registered`})
-    }
-    try{
-        let nuevoU= await Usuario.create({nombre,email,password,rol,estado})
-        const token=jwt.sign({id:nuevoU.id},'henribooks',{
-            expiresIn:86400//24 horas
-        })
-        res.json({token,usuario:nuevoU}) 
-    }catch(e){
-        res.status(404).send(e)
-    }
+const { Usuario } = require("../db");
+const jwt = require("jsonwebtoken");
+const { transporter } = require("../middleware/mail");
+const path = require("path");
+const filepath = path.join(__dirname, "../public/registerMail.html");
+const { Email } = process.env;
+
+async function signUp(req, res) {
+  let { nombre, email, password, rol, estado } = req.body;
+  let existe = await Usuario.findOne({ where: { nombre } });
+  if (existe) {
+    return res
+      .status(404)
+      .send({ msj: `User with name: ${nombre} already exist!` });
+  }
+  existe = await Usuario.findOne({ where: { email } });
+  if (existe) {
+    return res.status(404).send({ msj: `E-mail: ${email} already registered` });
+  }
+  try {
+    let nuevoU = await Usuario.create({ nombre, email, password, rol, estado });
+    const token = jwt.sign({ id: nuevoU.id }, "henribooks", {
+      expiresIn: 86400, //24 horas
+    });
+    const send = await transporter.sendMail({
+      from: `${Email}`, // sender address
+      to: email, // list of receivers
+      subject: "Registro Exitoso", // Subject line
+      html: { path: filepath }, // html body
+    });
+    res.json({ token, send, usuario: nuevoU });
+  } catch (e) {
+    res.status(404).send(e);
+  }
 }
 
+async function signIn(req, res) {
+  let { email, password } = req.body;
+  let encontrarUsuario = await Usuario.findOne({ where: { email } });
+  if (!encontrarUsuario) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  console.log(encontrarUsuario.password);
+  if (password != encontrarUsuario.password) {
+    return res.status(404).json({ message: "Wrong password" });
+  }
+  if (encontrarUsuario.estado == "desactivado") {
+    return res.status(404).json({ message: "User not found...." });
+  }
+  const token = jwt.sign({ id: encontrarUsuario.id }, "henribooks", {
+    expiresIn: 86400, //24 horas
+  });
+  const usuarioDatos = await Usuario.findByPk(encontrarUsuario.id);
 
-async function signIn (req,res){
-    let {email,password}=req.body
-    let encontrarUsuario = await Usuario.findOne({ where: {email} });
-    if(!encontrarUsuario){
-        return res.status(404).json({message:"User not found"})
-    }
-    console.log(encontrarUsuario.password)
-    if(password!=encontrarUsuario.password){
-        return res.status(404).json({message:"Wrong password"})
-    }
-    if(encontrarUsuario.estado=="desactivado"){
-        return res.status(404).json({message:"User not found...."}) 
-        
-    }
-    const token =jwt.sign({id:encontrarUsuario.id},'henribooks',{
-            expiresIn:86400//24 horas
-        })
-    const usuarioDatos=await Usuario.findByPk(encontrarUsuario.id)
-    
-    res.json({token,usuario:usuarioDatos})
+  res.json({ token, usuario: usuarioDatos });
 }
 
-module.exports={
-    signUp,
-    signIn
-}
+module.exports = {
+  signUp,
+  signIn,
+};
