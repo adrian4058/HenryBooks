9c942e5c0a928791ee52e78abe22c4132912d4f3
adrian4058 <adrian_2016_@outlook.es>
2023-04-04 18:56:19 -0300

    fix error auth0

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { DB_PORT } = process.env;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   console.log("conectado a base de datos");
   server.listen(DB_PORT, () => {
     console.log(`listening at port: ${DB_PORT}`); // eslint-disable-line no-console
