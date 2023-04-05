9d7b7991f3022afac563332607ad9f582b1ba79e
adrian4058 <adrian_2016_@outlook.es>
2023-04-05 15:52:25 -0300

    responsive design team

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-container">
-
         <div className="footer-content">
           <div className="footer-column">
             <h4>Contact</h4>
@@ -15,46 +14,50 @@ function Footer() {
 
           <div className="footer-column">
             <h4>Follow</h4>
-            <a href='https://www.facebook.com/people/Henry-Book/100089922381588/'
-              target='_blank'
-              rel='noreferrer'>
-              <ion-icon name='logo-facebook' /> Facebook
+            <a
+              href="https://www.facebook.com/people/Henry-Book/100089922381588/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-facebook" /> Facebook
             </a>
-            <a href='https://www.instagram.com/henrybooks_pf/'
-              target='_blank'
-              rel='noreferrer'>
-
-              <ion-icon name='logo-instagram' /> Instagram
+            <a
+              href="https://www.instagram.com/henrybooks_pf/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-instagram" /> Instagram
             </a>
-            <a href='https://twitter.com/HenryBooks_PF'
-              target='_blank'
-              rel='noreferrer'>
-              <ion-icon name='logo-twitter' /> Twitter
+            <a
+              href="https://twitter.com/HenryBooks_PF"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ion-icon name="logo-twitter" /> Twitter
             </a>
           </div>
 
           <div className="footer-column">
             <h4>Navigation</h4>
-            <Link to='/home'>
+            <Link to="/home">
               <p className="navegationText">Home</p>
             </Link>
-            <Link to='/team'>
+            <Link to="/team">
               <p className="navegationText">Team</p>
             </Link>
-            <Link to='/about-us'>
+            <Link to="/about-us">
               <p className="navegationText">About us</p>
             </Link>
-            <Link to='/preguntas'>
+            <Link to="/preguntas">
               <p className="navegationText">FaQs</p>
             </Link>
-
           </div>
         </div>
 
-        <p className='footer-copy'>Ⓒ Henry Books | All Rights Reserved 2023 </p>
+        <p className="footer-copy">Ⓒ Henry Books | All Rights Reserved 2023 </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
